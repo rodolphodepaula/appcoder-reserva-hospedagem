b854c953a9e19d6ab6953385f8946b2ebcbaa759
rodolphosp <rodolphosp@gmail.com>
2023-12-05 20:38:47 -0300

    Criando rota, controller, seeder e balde

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = []; // Qualquer novo campo é protegido.
 
     /**
      * The attributes that should be hidden for serialization.
