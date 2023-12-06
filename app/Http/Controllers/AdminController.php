edb688335956218700c5541e79ed9c1018c02985
rodolphosp <rodolphosp@gmail.com>
2023-12-06 02:42:03 -0300

    Tela de login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function adminDashboard()
     {
         return view('admin.index');
+    }
+
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+
+    public function login()
+    {
+        return view('admin.admin_login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -34,5 +34,9 @@ require __DIR__.'/auth.php';
 //Admin Group Middleware
 Route::middleware(['auth', 'roles:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'adminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
+
 });
 //End Admin Group Middleware
+
+Route::get('/admin/login', [AdminController::class, 'login'])->name('admin.login');
